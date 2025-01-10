062649c825816b3572c43f00249381c1196ec4b7
Kye <NovusAa12@proton.me>
2025-01-10 11:02:01 +0000

    chore: initial commit

---
+++ src/main/java/com/hampshirewolves/aeroatlasapi/AeroAtlasApiApplication.java
@@ -0,0 +1,13 @@
+package com.hampshirewolves.aeroatlasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroAtlasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroAtlasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hampshirewolves/aeroatlasapi/AeroAtlasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hampshirewolves.aeroatlasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroAtlasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
