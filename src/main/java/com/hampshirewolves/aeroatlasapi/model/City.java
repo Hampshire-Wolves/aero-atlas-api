9c9ca90a988afa4f8185b24bed6af58f711561b0
Kye <NovusAa12@proton.me>
2025-01-10 15:43:06 +0000

    feat: add City model

---
+++ src/main/java/com/hampshirewolves/aeroatlasapi/model/City.java
@@ -0,0 +1,61 @@
+package com.hampshirewolves.aeroatlasapi.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "cities")
+@Entity
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false, name = "image_url")
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String lat;
+
+    @Column(nullable = false)
+    private String lng;
+
+    @Column(nullable = false)
+    private String iataCode;
+
+    @Column(nullable = false, name = "star_rating")
+    private StarRating starRating;
+
+    @Column(nullable = false, name = "price_rating")
+    private PriceRating priceRating;
+
+    @Column(name = "created_at", updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "modified_at")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
+    @UpdateTimestamp
+    private LocalDateTime modifiedAt;
+}
