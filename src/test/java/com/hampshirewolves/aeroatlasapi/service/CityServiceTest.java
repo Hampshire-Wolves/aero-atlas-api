f2f312c24adc7a384b5d898762d05cd4f1fb977d
Kye <NovusAa12@proton.me>
2025-01-13 15:34:33 +0000

    test(CityServiceImpl): add test for getAllCities

--- src/test/java/com/hampshirewolves/aeroatlasapi/service/CityServiceTest.java
+++ src/test/java/com/hampshirewolves/aeroatlasapi/service/CityServiceTest.java
@@ -1,15 +1,53 @@
 package com.hampshirewolves.aeroatlasapi.service;
 
+import com.hampshirewolves.aeroatlasapi.model.City;
 import com.hampshirewolves.aeroatlasapi.repository.CityRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
 @SpringBootTest
 public class CityServiceTest {
     @Mock
-    private CityRepository cityRepository;
+    private CityRepository mockCityRepository;
 
     @InjectMocks
-    private CityServiceImpl cityServiceImpl;
+    private CityServiceImpl mockCityServiceImpl;
+
+    @Test
+    @DisplayName("getAllCities: should return all cities")
+    public void testGetAllCities() {
+        List<City> cityList = new ArrayList<>();
+        cityList.add(City.builder()
+                .name("London")
+                .build());
+        cityList.add(City.builder()
+                .name("Berlin")
+                .build());
+        cityList.add(City.builder()
+                .name("Madrid")
+                .build());
+
+        when(mockCityRepository.findAll()).thenReturn(cityList);
+
+        List<City> actualResult = mockCityServiceImpl.getAllCities();
+        City city1 = actualResult.getFirst();
+        City city2 = actualResult.get(1);
+        City city3 = actualResult.getLast();
+
+        assertThat(actualResult).hasSize(3);
+        assertThat(city1).hasFieldOrPropertyWithValue("name", "London");
+        assertThat(city2).hasFieldOrPropertyWithValue("name", "Berlin");
+        assertThat(city3).hasFieldOrPropertyWithValue("name", "Madrid");
+
+        verify(mockCityRepository, times(1)).findAll();
+    }
 }
