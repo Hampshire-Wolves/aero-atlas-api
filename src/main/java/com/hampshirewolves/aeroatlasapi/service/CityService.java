4a7b0f172ef62754ae42990f8db7737bbe49c483
Kye <NovusAa12@proton.me>
2025-01-13 15:36:53 +0000

    feat: add CityService interface

---
+++ src/main/java/com/hampshirewolves/aeroatlasapi/service/CityService.java
@@ -0,0 +1,9 @@
+package com.hampshirewolves.aeroatlasapi.service;
+
+import com.hampshirewolves.aeroatlasapi.model.City;
+
+import java.util.List;
+
+public interface CityService {
+    List<City> getAllCities();
+}
