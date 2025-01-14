b68b77830bd3324b6c01d4576bedcbc05b10eb46
Kye <NovusAa12@proton.me>
2025-01-14 16:47:51 +0000

    feat: add getAllCities to CityController

---
+++ src/main/java/com/hampshirewolves/aeroatlasapi/controller/CityController.java
@@ -0,0 +1,25 @@
+package com.hampshirewolves.aeroatlasapi.controller;
+
+import com.hampshirewolves.aeroatlasapi.model.City;
+import com.hampshirewolves.aeroatlasapi.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cities")
+public class CityController {
+
+    @Autowired
+    private CityService cityService;
+
+    @GetMapping
+    public ResponseEntity<List<City>> getAllCities() {
+        List<City> albums = cityService.getAllCities();
+
+        return new ResponseEntity<>(albums, HttpStatus.OK);
+    }
+}
