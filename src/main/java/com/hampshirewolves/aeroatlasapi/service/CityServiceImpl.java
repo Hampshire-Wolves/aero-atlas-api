2830722f0000d1a637ee78abc991deac0711e023
Kye <NovusAa12@proton.me>
2025-01-14 11:59:10 +0000

    feat: add addCity to CityService & CityServiceImpl

--- src/main/java/com/hampshirewolves/aeroatlasapi/service/CityServiceImpl.java
+++ src/main/java/com/hampshirewolves/aeroatlasapi/service/CityServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hampshirewolves.aeroatlasapi.service;
 
 import com.hampshirewolves.aeroatlasapi.exception.CityNotFoundException;
+import com.hampshirewolves.aeroatlasapi.exception.MissingFieldException;
 import com.hampshirewolves.aeroatlasapi.model.City;
 import com.hampshirewolves.aeroatlasapi.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +28,28 @@ public class CityServiceImpl implements CityService {
     public City getCityById(Long id) {
         return cityRepository.findById(id)
                 .orElseThrow(() -> new CityNotFoundException(String.format("City with id '%s' could not be found", id)));
+    }
+
+    @Override
+    public City addCity(City city) {
+        boolean hasValidFields = requestBodyHasValidFields(city);
+
+        if (!hasValidFields) {
+            throw new MissingFieldException("Missing field(s) in request body");
+        }
+
+        return cityRepository.save(city);
+    }
+
+    private boolean requestBodyHasValidFields(City city) {
+        if (city.getName() == null || city.getDescription() == null
+                || city.getImageUrl() == null || city.getCountry() == null
+                || city.getLat() == null || city.getLng() == null
+                || city.getIataCode() == null || city.getStarRating() == null
+        || city.getPriceRating() == null) {
+            return false;
+        }
+
+        return true;
     }
 }
